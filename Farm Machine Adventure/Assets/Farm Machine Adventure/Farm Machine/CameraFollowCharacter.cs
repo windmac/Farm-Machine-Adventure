031953c295e87312593d4ceb9f74ce48f1673b3b
Klein Mac <zhaomao.mai@efrei.net>
2018-12-21 16:47:38 +0800

    First commit
    Simple movement has done

---
+++ Farm Machine Adventure/Assets/Farm Machine Adventure/Farm Machine/CameraFollowCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowCharacter : MonoBehaviour
+{
+    public GameObject camara;
+    public GameObject player;
+    // private Vector3 offset;
+    public float offset;
+    private Vector3 PlayerPosition;
+    public float SmoothingTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //offset = camara.transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // camara.transform.position = player.transform.position + offset;
+
+        PlayerPosition = new Vector3(player.transform.position.x, camara.transform.position.y, camara.transform.position.z);
+
+        if(player.transform.localScale.x>0f)
+        {
+            PlayerPosition = new Vector3(PlayerPosition.x + offset, PlayerPosition.y, PlayerPosition.z);
+        }
+        else
+        {
+            PlayerPosition = new Vector3(PlayerPosition.x - offset, PlayerPosition.y, PlayerPosition.z);
+        }
+
+        camara.transform.position = Vector3.Lerp(camara.transform.position, PlayerPosition, SmoothingTime*Time.deltaTime);
+
+       // camara.transform.position = PlayerPosition;
+    }
+}
---
+++ Farm Machine Adventure/Assets/Farm Machine Adventure/Farm Machine/FarmMachineController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmMachineController : MonoBehaviour
+{
+    public float speed =10f;
+    public float jumpspeed = 100f;
+    private float movement = 0f;
+    private Rigidbody2D rigidbody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+    private Animator animation;
+    private Vector2 scale_temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animation = GetComponent<Animator>();
+        scale_temp = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        movement = Input.GetAxis("Horizontal");
+
+
+
+        if (movement > 0f)
+        {
+            rigidbody.velocity = new Vector2(movement * speed, rigidbody.velocity.y);
+            animation.SetBool("IsWalking", true);
+            transform.localScale = new Vector2(scale_temp.x, scale_temp.y);
+        }
+        else if(movement < 0f)
+        {
+            rigidbody.velocity = new Vector2(movement * speed, rigidbody.velocity.y);
+            animation.SetBool("IsWalking", true);
+            transform.localScale = new Vector2(-scale_temp.x, scale_temp.y);
+        }
+        else
+        {
+            animation.SetBool("IsWalking", false);
+        }
+        
+
+        if(Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpspeed);
+           
+        }
+
+        if (Input.GetKey("down") && isTouchingGround)
+        {
+            animation.SetBool("IsSitting", true);
+        }
+        else
+        {
+            animation.SetBool("IsSitting", false);
+        }
+
+        animation.SetBool("OnGround", isTouchingGround);
+    }
+}
